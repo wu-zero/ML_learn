c99e03f33340e523b9ed65c5e6f0af6856fea73f
wu-zero <wangyingwu@126.com>
2019-02-24 22:41:38 +0800

    change day1

--- code/day1.py
+++ code/day1.py
@@ -36,9 +36,7 @@ Y = labelencoder_Y.fit_transform(Y)
 
 # 5. 拆分数据集
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(
-    X, Y, test_size=0.2, random_state=0)
-
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 # print(X_train)
 # print(X_test)
 # print('='*20)
@@ -48,7 +46,6 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
-
-print(X_train)
-print(X_test)
-print('='*20)
+# print(X_train)
+# print(X_test)
+# print('='*20)
